a3b5e932475d9b63c7101779f7c93d9fd74abf84
Christian Garcia <bones192nd@gmail.com>
2023-06-01 17:37:05 -0400

    didn't finish the full lab

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,60 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__ (self, name="", breed = ""):
+        self.name = name
+        self.breed = breed
+
+    def get_name (self):
+        return self._name
+    
+    def set_name (self, name):
+        if name == "" or not isinstance(name, str) or len(name) > 25:
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            self._name = name
+
+    # def set_breed (self, breed):
+    #     # if breed not in APPROVED_BREEDS:
+    #     #     print("Breed must be in list of approved breeds.")
+    #     # else:
+    #     #     self._breed = breed
+    #     print("Breed must be in list of approved breeds")
+    #     self._breed = breed
+
+    def get_breed (self):
+        return self._breed
+    
+    name = property(get_name, set_name)
+    # breed = property(get_breed, set_breed)
+
+    # def __init__(self, name="", breed=""):
+    #     if breed in APPROVED_BREEDS:
+    #         print("Breed must be in list of approved breeds.")
+
+    #     if name == "" or not isinstance(name, str) or len(name) > 25:
+    #         print("Name must be string between 1 and 25 characters.")
+    #     else:
+    #         self.name = name
+
+    #     self.breed = breed
+
+    # def set_breed(self, breed):
+    #     if breed not in APPROVED_BREEDS:
+    #         print("Breed must be in list of approved breeds.")
+    #     self.breed = breed
+
+    
+
+    # def set_na)me(self):
+    #     if name == "":
+    #         print("Name must be string between 1 and 25 characters.")
+    #     else:
+    #         self._name = name
+
+    # breed = property(set_breed)
+
+
+# cliff = Dog("hola")
+
+# print(cliff)
